7db47166e8a7d9b58f563ef5ffebaeacc12298a8
Vasant Bedadala <vasant.reddy@gmail.com>
2013-08-28 00:13:23 -0400

    Merge sort in python

---
+++ algorithms-python/sorting/Mergesort.py
@@ -0,0 +1,65 @@
+'''
+Implementation of merge sort using python
+Worst case : theta(nlogn)
+
+This algorithm does not sort in place
+Space complexity : n
+@author: vasant
+'''
+import math
+import random
+import datetime
+
+def merge(left, right):
+    
+    result = [] 
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+                result.append(left[i])
+                i += 1
+        else:
+                result.append(right[j])
+                j += 1
+    
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+def mergesort(unsorted):
+    if len(unsorted) <= 1 :
+        return unsorted
+    mid = int(math.floor(len(unsorted) / 2))
+    
+    left = mergesort(unsorted[:mid])
+    right = mergesort (unsorted[mid:])
+    
+    return merge(left, right)
+    
+def printList(lst):
+    for i in lst:
+        print lst[i]
+
+def __main__():
+    unsorted = []
+    for i in range(100):
+        unsorted.append(i)
+        random.shuffle(unsorted)
+
+    printList(unsorted)
+    print "start time $d", (datetime.datetime.now())
+    unsorted = mergesort(unsorted) 
+    print "end time $d", (datetime.datetime.now())
+    print "After sorting"
+    printList(unsorted)
+
+if __name__ == '__main__':  
+    __main__()
+
+
+
+             
+        
+    
+    
+    
