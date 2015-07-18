8b645fbb8f565e2697c8006bcd15a53e089fbc73
System Administrator <root@Vasants-iMac.local>
2015-07-18 13:39:50 -0400

    changes

--- algorithms-python/divideAndConqu/Power.py
+++ algorithms-python/divideAndConqu/Power.py
@@ -1,28 +1,28 @@
 '''
 Created on Sep 18, 2013
-
+Recursive power
 @author: vasant
 '''
 
 memory = dict()
-def powerOfN(x,n):
-    if n==0:
+
+
+def powerOfN(x, n):
+    if n == 0:
         return 1
-    if n==1:
+    if n == 1:
         return x
     else:
-        if n%2==0:
-            val = powerOfN(x,n/2)
+        if n % 2 == 0:
+            val = powerOfN(x, n / 2)
             return val * val
         else:
-            val = powerOfN(x,(n-1)/2)
+            val = powerOfN(x, (n - 1) / 2)
             return val * val * x
-    
-
 
 
 def __main__():
     print "2 Power 10000 is " + str(powerOfN(2, 1000000))
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
     __main__()
