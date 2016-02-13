c6520ee158e34a36ea51033f27f166b0422489a1
Vasant Bedadala <vasant.reddy@gmail.com>
2016-02-13 16:23:48 -0500

    sorting

--- Algorithms/src/sorting/MergeSort.java
+++ Algorithms/src/sorting/MergeSort.java
@@ -6,55 +6,46 @@ public class MergeSort implements Sort {
 
     @Override
     public int[] sort(int[] input) {
-	input = mergeSort(input);
-	for (int i = 0; i < input.length; i++) {
-	    System.out.println(input[i] + "    ");
-	}
-	return input;
+        input = mergeSort(input, new int[input.length], 0, input.length);
+        for (int i = 0; i < input.length; i++) {
+            System.out.println(input[i] + "    ");
+        }
+        return input;
     }
 
-    private int[] mergeSort(int[] input) {
-	if (input.length <= 1) {
-	    return input;
-	}
-	int[] mid1 = mergeSort(Arrays.copyOfRange(input, 0,
-		(int) Math.floor(input.length / 2)));
-	int[] mid2 = mergeSort(Arrays.copyOfRange(input,
-		(int) Math.floor(input.length / 2), (input.length)));
-	int[] merged = merge(mid1, mid2);
-	return merged;
-
+    private int[] mergeSort(int[] input, int[] aux, int start, int end) {
+        if (start >= end) {
+            return input;
+        }
+        int mid = start + (end - start) / 2;
+        mergeSort(input, aux, start, mid);
+        mergeSort(input, aux, mid + 1, end);
+        merge(input, aux, start, mid, end);
+        return input;
     }
 
-    private int[] merge(int[] input1, int[] input2) {
-	int[] merged = new int[input1.length + input2.length];
-	int i = 0;
-	int j = 0;
-	int r = 0;
-	while (i < input1.length && j < input2.length) {
-	    if (input1[i] <= input2[j]) {
-		merged[r] = input1[i];
-		r++;
-		i++;
-	    } else if (input1[i] > input2[j]) {
-		merged[r] = input2[j];
-		r++;
-		j++;
-	    }
-
-	}
-	while (j < input2.length) {
-	    merged[r] = input2[j];
-	    r++;
-	    j++;
-	}
-
-	while (i < input1.length) {
-	    merged[r] = input1[i];
-	    r++;
-	    i++;
-	}
-	return merged;
+    private void merge(int[] input, int[] aux, int start, int mid, int end) {
+        //copy to aux
+        for (int k = start; k < end; k++) {
+            aux[k] = input[k];
+        }
+        int i = start;
+        int j = mid + 1;
+        for (int k = start; k < end; k++) {
+            if (i >= mid) {
+                input[k] = aux[j];
+                j++;
+            } else if (j >= end) {
+                input[k] = aux[i];
+                i++;
+            } else if (aux[i] < aux[j]) {
+                input[k] = aux[i];
+                i++;
+            } else {
+                input[k] = aux[j];
+                j++;
+            }
+        }
     }
 
 }
--- Algorithms/src/sorting/QuickSort.java
+++ Algorithms/src/sorting/QuickSort.java
@@ -1,60 +1,40 @@
-//package sorting;
-//
-//import java.util.Arrays;
-//
-//public class MergeSort implements Sort {
-//
-//    @Override
-//    public int[] sort(int[] input) {
-//	input = mergeSort(input);
-//	for (int i = 0; i < input.length; i++) {
-//	    System.out.println(input[i] + "    ");
-//	}
-//	return input;
-//    }
-//
-//    private int[] mergeSort(int[] input) {
-//	if (input.length <= 1) {
-//	    return input;
-//	}
-//	int[] mid1 = mergeSort(Arrays.copyOfRange(input, 0,
-//		(int) Math.floor(input.length / 2)));
-//	int[] mid2 = mergeSort(Arrays.copyOfRange(input,
-//		(int) Math.floor(input.length / 2), (input.length)));
-//	int[] merged = merge(mid1, mid2);
-//	return merged;
-//
-//    }
-//
-//    private int[] merge(int[] input1, int[] input2) {
-//	int[] merged = new int[input1.length + input2.length];
-//	int i = 0;
-//	int j = 0;
-//	int r = 0;
-//	while (i < input1.length && j < input2.length) {
-//	    if (input1[i] <= input2[j]) {
-//		merged[r] = input1[i];
-//		r++;
-//		i++;
-//	    } else if (input1[i] > input2[j]) {
-//		merged[r] = input2[j];
-//		r++;
-//		j++;
-//	    }
-//
-//	}
-//	while (j < input2.length) {
-//	    merged[r] = input2[j];
-//	    r++;
-//	    j++;
-//	}
-//
-//	while (i < input1.length) {
-//	    merged[r] = input1[i];
-//	    r++;
-//	    i++;
-//	}
-//	return merged;
-//    }
-//
-//}
+package sorting;
+
+import java.util.Random;
+
+public class QuickSort{
+
+  private Random random = new Random();
+
+    public int[] qsort(int[] input, int p, int q) {
+
+        if(p<q) {
+            int pivot = partition(input, p, q);
+            qsort(input, p, pivot-1);
+            qsort(input, pivot + 1, q);
+        }
+        return input;
+    }
+
+    private int partition(int [] x, int p, int q) {
+        int pivot = random.nextInt(q-p + 1) + p;
+
+        swap(q,pivot,x);
+
+        int i=p-1;
+       for(int j=p; j<q; j++) {
+           if(x[j]<=x[q]) {
+             i++;
+             swap(i,j,x);
+           }
+       }
+        swap(q,i+1,x);
+        return i+1;
+    }
+
+    private void swap(int i, int j, int[] x) {
+        int temp=x[i];
+        x[i]=x[j];
+        x[j]=temp;
+    }
+}
